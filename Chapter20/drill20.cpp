c11d7192b5f56dabc1323a13f2284fe51c31f1c7
BPetra10 <bodnar.petra9@gmail.com>
2022-03-26 18:46:44 +0100

    drill20 final

--- Chapter20/drill20.cpp
+++ Chapter20/drill20.cpp
@@ -12,7 +12,24 @@
 
 using namespace std; 
 
-//6.
+//template for printing:
+template<typename T> void write(const T& t, string s)
+{
+  cout<< s <<" ";
+  for(auto& x : t)
+    cout<< x <<" ";
+  cout<<"\n";
+}
+
+//template for summation:
+template<typename T>
+void increase(T& t, int plus)
+{
+    for (auto& a : t)
+        a += plus;
+}
+
+//6. template for copy:
 template<typename Iter1, typename Iter2> Iter2 my_copy(Iter1 f1, Iter1 e1, Iter2 f2)
 {
   if(f1!=e1)
@@ -31,26 +48,16 @@ int main()
 try{
   //1.
   int array1 [10] = {0,1,2,3,4,5,6,7,8,9};
-  cout<<"Array: ";
-  for(auto x : array1)
-    cout<< x <<" ";
-  cout<<"\n";
-  
+  write(array1,"Array:");
   //2.
   vector<int> v {0,1,2,3,4,5,6,7,8,9};
-  cout<<"Vector: ";
-  for(auto velem : v)
-    cout<< velem <<" ";
-  cout<<"\n";
-
+  write(v,"Vector:");
   //3.
   list<int> list1 = {0,1,2,3,4,5,6,7,8,9};
-  cout<<"List: ";
-  for(auto lelem : list1)
-    cout<< lelem <<" ";
-  cout<<"\n \n";
+  write(list1,"List:");
+  cout<<"\n";
 
-  //4.
+  //4. copy with = 
   int array2[10];
   for(int i=0; i<10;++i)
     array2[i]=array1[i];
@@ -58,37 +65,27 @@ try{
   vector<int> v2 = v;
   list<int> list2 = list1;
 
-  cout<<"Array copy: ";
-  for(auto x2 : array2)
-    cout<< x2 <<" ";
+  write(array2,"Array copy:");
+  write(v2,"Vector copy:");
+  write(list2,"List copy:");
   cout<<"\n";
-
-  cout<<"Vector copy: ";
-  for(auto velem2 : v2)
-    cout<< velem2 <<" ";
-  cout<<"\n";
-
-  cout<<"List copy: ";
-  for(auto lelem2 : list2)
-    cout<< lelem2 <<" ";
-  cout<<"\n \n";
 
   //5.
-  for(int i=0;i<10;++i)
-    array1[i]+=2;
-
-  cout<<"Array increased by 2: ";
-  for(auto x : array1)
-    cout<< x <<" ";
+  increase(array1,2);
+  write(array1,"Array increased by 2:");
+  increase(v,3);
+  write(v,"Vector increased by 3:");
+  increase(list1,5);
+  write(list1,"List increased by 5:");
   cout<<"\n";
 
+  /*for(int i=0;i<10;++i)
+    array1[i]+=2;
+  write(array1,"Array increased by 2:");
+  
   for(int i=0; i<v.size(); ++i)
       v[i]+=3;
-
-  cout<<"Vector increased by 3: ";
-  for(auto x : v)
-    cout<< x <<" ";
-  cout<<"\n";
+  write(v,"Vector increased by 3:");
 
   list<int>::iterator p = list1.begin();
   if(p==list1.end())
@@ -96,15 +93,12 @@ try{
   else
   {
     while(p != list1.end()) 
-    {
-      *p = *p+5;
-      p++;
-    }
+    {*p = *p+5;
+      p++;}
   }
-  cout<<"List increased by 5: ";
-  for(auto x : list1)
-    cout<< x <<" ";
-  cout<<"\n \n";
+  write(list1,"List increased by 5:");
+  */
+  
   //7.
   //copy array into vector
   vector<int> v3(10);
@@ -115,29 +109,21 @@ try{
   array<int,10> array4;
   my_copy(list1.begin(),list1.end(),array4.begin());
 
-  cout<<"Copy() array into vector: ";
-  for(auto x : v3)
-    cout<< x <<" ";
+  write(v3,"Copy() array into vector:");
+  write(array4,"Copy() list into array:");
   cout<<"\n";
 
-  cout<<"Copy() list into array: ";
-  for(auto x : array4)
-    cout<< x <<" ";
-  cout<<"\n \n";
-
-  //8.
+  //8. find()
   auto f_3_vec = find(v.begin(),v.end(),3);
   auto f_27_list = find(list1.begin(),list1.end(),27);
 
-  if(f_3_vec!=v.end()) 
-    cout << "Vector contains 3. \n";
-  else 
-    cout << "Vector doesn't contain 3. \n";
+  (f_3_vec!=v.end()) 
+   ? cout << "Vector contains 3. \n"
+   : cout << "Vector doesn't contain 3. \n";
 
-  if(f_27_list!=list1.end()) 
-    cout << "List contains 27. \n";
-  else 
-    cout << "List doesn't contain 27. \n";
+  (f_27_list!=list1.end()) 
+   ? cout << "List contains 27. \n" 
+   : cout << "List doesn't contain 27. \n";
 }
 catch(std::exception& e) {
     std::cerr << "Exception: " << e.what() << '\n';
