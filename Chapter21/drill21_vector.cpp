661d5ac9f1517e16d425410ca8387eaffb0c97b2
BPetra10 <bodnar.petra9@gmail.com>
2022-04-06 20:20:10 +0200

    drill21_vec

--- Chapter21/drill21_vector.cpp
+++ Chapter21/drill21_vector.cpp
@@ -31,99 +31,67 @@ istream& operator>>(istream& is, Item& it)
     return is;
 }
 
-//6.
-class Erease_by_name
+//template for printing
+template<typename C>
+void write(C& c, string s)
 {
-	string name;
-public:
-	//konstruktor
-	Erease_by_name(string nname): name{nname}{}
-	//függvényhívó operátor (function call) túlterhelése, 
-	//mely elvégzi az összehasonlítást.
-	bool operator()(Item& x) const {return x.name==name;}
-};
-
-//7.
-class Erease_by_id
-{
-	int id;
-public:
-	//konstruktor
-	Erease_by_id(int iid): id{iid}{}
-	//függvényhívó operátor (function call) túlterhelése, 
-	//mely elvégzi az összehasonlítást.
-	bool operator()(Item& x) const {return x.iid==id;}
-};
+    cout<<s<<"\n";
+    for (auto p = c.begin();p!=c.end(); ++p)
+        cout<<*p;
+    cout<<"\n";
+}
 
 int main()
-{
+try{
     vector<Item> vi;
     string iname {"items.txt"};
     ifstream ist {iname};
     if (!ist) runtime_error("can't open input file "+ iname);
 
     for (Item it; ist >> it; ) vi.push_back(it);
-
-    cout<<"Original vector:"<<"\n";
-    for(auto& x : vi)
-        cout << x;
-    cout<<endl;
+    
+    write(vi,"Original vector:");
 
     //2.
     string a,b;
 
     sort(vi.begin(), vi.end(),
 		[](const Item& a, const Item& b){return a.name < b.name;});
-    
-	cout << "After sort by name:\n";
-    for(auto& x : vi)
-        cout<< x;
-    cout<<endl;
 
+    write(vi,"After sort by name:");
+	
     //3.
     sort(vi.begin(), vi.end(),
 		[](const Item& a, const Item& b){return a.iid < b.iid;});
-
-    cout << "After sort by iid:\n";
-    for(auto& x : vi)
-        cout<< x;
-    cout<<endl;
+    
+    write(vi,"After sort by id:");
 
     //4.
     sort(vi.begin(), vi.end(),
 		[](const Item& a, const Item& b){return a.value > b.value;});
-
-    cout << "After sort by value:\n";
-    for(auto& x : vi)
-        cout<< x;
-    cout<<endl;
+    
+    write(vi,"After sort by value:");
 
     //5.
     Item horse {"horse shoe",99,12.34};
     Item canon {"Canon S400",9988,499.95};
     vi.insert(vi.begin(), horse);
     vi.insert(vi.begin(), canon);
-    
-    cout << "After insert:\n";
-     for(auto& x : vi)
-        cout<< x;
-    cout<<endl;
+    write(vi,"After insert:");
 
     //6.
-    string name="Józsi";
-    vi.erase(find_if(vi.begin(),vi.end(),Erease_by_name(name)));
-    cout << "Remove by name:\n";
-     for(auto& x : vi)
-        cout<< x;
-    cout<<endl;
+    string name="horse shoe";
+    vi.erase(find_if(vi.begin(),vi.end(),
+        [name](const Item& a){return a.name == name;}));
+    
+    write(vi,"Remove by name:");
 
     //7.
-    int id=1;
-    vi.erase(find_if(vi.begin(),vi.end(),Erease_by_id(id)));
-    cout << "Remove by id:\n";
-     for(auto& x : vi)
-        cout<< x;
-    cout<<endl;
+    int id=9988;
+    vi.erase(find_if(vi.begin(),vi.end(),
+        [id](const Item& a){return a.iid == id;}));
+
+    write(vi,"Remove by id:");
 
     //8. Repeat the excercise with list:
     //1.
@@ -133,60 +101,53 @@ int main()
     if (!ist) runtime_error("can't open input file "+ iname2);
 
     for (Item it; ist2 >> it; ) li.push_back(it);
-
-    cout<<"Original list:"<<"\n";
-    for(auto& x : li)
-        cout << x;
-    cout<<endl;
+    
+    write(li,"Original list:");
 
     //2.
     string a2,b2;
     li.sort([](const Item& a2, const Item& b2){return a2.name < b2.name;});
-	cout << "After sort by name:\n";
-    for(auto& x : li)
-        cout<< x;
-    cout<<endl;
+    write(li,"After sort by name:");
 
     //3.
     li.sort([](const Item& a2, const Item& b2){return a2.iid < b2.iid;});
-    cout << "After sort by iid:\n";
-    for(auto& x : li)
-        cout<< x;
-    cout<<endl;
-
+    write(li,"After sort by id:");
+    
     //4.
-    li.sort([](const Item& a2, const Item& b2){return a2.value < b2.value;});
-    cout << "After sort by value:\n";
-    for(auto& x : li)
-        cout<< x;
-    cout<<endl;
+    li.sort([](const Item& a2, const Item& b2){return a2.value > b2.value;});
+    write(li,"After sort by value:");
 
     //5.
     li.insert(li.begin(), horse);
     li.insert(li.begin(), canon);
-    cout << "After insert:\n";
-     for(auto& x : li)
-        cout<< x;
-    cout<<endl;
-
+    write(li,"After insert:");
+    
     //6.
     string name2="András";
-    // Creating iterator to point to the element wich we want to delete
-    list<Item>::iterator itr = find_if(li.begin(),li.end(),Erease_by_name(name2));
+
+    list<Item>::iterator itr = 
+    find_if(li.begin(),li.end(),
+        [name2](const Item& a){return a.name == name2;});
+    
     li.erase(itr);
-    cout << "Remove by name:\n";
-     for(auto& x : li)
-        cout<< x;
-    cout<<endl;
+    write(li,"Remove by name:");
 
     //7.
     int id2=5;
-    itr=find_if(li.begin(),li.end(),Erease_by_id(id2));
+
+    itr=find_if(li.begin(),li.end(),
+        [id2](const Item& a){return a.iid == id2;});
+
     li.erase(itr);
-    cout << "Remove by id:\n";
-     for(auto& x : li)
-        cout<< x;
-    cout<<endl;
+    write(li,"Remove by id:");
 
     return 0;
+}
+catch(std::exception& e) {
+    std::cerr << "Exception: " << e.what() << '\n';
+    return 1;
+}
+catch(...) {
+    std::cerr << "Something went wrong! \n";
+    return 2;
 }
